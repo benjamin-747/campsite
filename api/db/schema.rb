2c13b1dd332c6c53f260c3a1fd3465630481aa8f
benjamin.747 <benjamin.747@outlook.com>
2026-01-30 10:31:17 +0800

    fix: update demo env encrypt key and add org init migrate

--- api/db/schema.rb
+++ api/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2024_12_12_002307) do
+ActiveRecord::Schema[7.2].define(version: 2026_01_29_075537) do
   create_table "attachments", id: { type: :bigint, unsigned: true }, charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "public_id", limit: 12, null: false
     t.text "file_path", null: false
